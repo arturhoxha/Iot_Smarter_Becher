de51f8ed6f29ff2a2daf7716aa719d3df7beb8e4
arturhoxha <artur.hoxha@hotmail.de>
2024-12-05 23:20:30 +0100

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-export default {
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()], // Vue-Plugin einfügen
   server: {
-      proxy: {
-          '/api': 'http://localhost:7071', // Lokale Azure Functions URL
-      },
+    proxy: {
+      '/api': 'http://localhost:7071', // Lokale Azure Functions URL
+    },
   },
-};
+});
